f94837d6c0316419a55a5d013f0f0a2ee2d30d87
Pavel Zarudnev <pavel@MacBook-Air.local>
2015-04-15 22:19:40 +0300

    Initial Commit

---
+++ PubNub_chatTests/PubNub_chatTests.m
@@ -0,0 +1,40 @@
+//
+//  PubNub_chatTests.m
+//  PubNub_chatTests
+//
+//  Created by Pavel on 15.04.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PubNub_chatTests : XCTestCase
+
+@end
+
+@implementation PubNub_chatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
